8a256a8364ee1082330680c450b1c8d438c765fb
D-Savel <dad.savel@gmail.com>
2021-05-17 18:08:13 +0200

    Update Todos reducer & context

--- src/components/AddTodoForm.js
+++ src/components/AddTodoForm.js
@@ -1,24 +1,38 @@
-const AddTodoForm = (props) => {
-  const { addTodo } = props
+
+import { useTodosDispatch } from "../context/TodosDispatchContext";
+
+const AddTodoForm = () => {
+
+  const dispatch = useTodosDispatch();
+  const addTodo = (text) => {
+    const newTodo = {
+      text,
+      isCompleted: false,
+    };
+    // setTodos([...todos, newTodo]);
+    dispatch({ type: "ADD", payload: newTodo });
+  };
+
+
   const handleFormSubmit = (event) => {
-    event.preventDefault()
-    const newTodoText = event.target.elements.todo.value
-    addTodo(newTodoText)
-    event.target.reset()
-  }
+    event.preventDefault();
+    const newTodoText = event.target.elements.todo.value;
+    addTodo(newTodoText);
+    event.target.reset();
+  };
   return (
     <form onSubmit={handleFormSubmit}>
       <div className="input-group mb-2">
         <label className="input-group-text" htmlFor="todo">
           Ajouter une tâche
-        </label>
+            </label>
         <input className="form-control" id="todo" required />
       </div>
       <button type="submit" className="btn btn-primary">
         allons-y !
-      </button>
+          </button>
     </form>
-  )
-}
+  );
+};
 
-export default AddTodoForm
+export default AddTodoForm;
